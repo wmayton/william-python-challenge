22ccceb502830a75b70d5dbe184bf2c548e89537
William Mayton <william_mayton@Williams-MacBook-Air.local>
2021-04-05 13:10:03 -0400

    python challenge update

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,10 @@ import os
 #import module for reading CSV files
 import csv
 csvpath = os.path.join('Resources', 'PyBank.csv')
-with open(csvpath, 'r') as file_handler:
-    lines= file_handler.read()
-    print(lines)
-    print(type(lines))
+total_months = -1
+for row in open(csvpath):
+    total_months += 1
+print (total_months)
+with open (csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
