dc4e944b00dd3c457bb6a380f3098278fe9094a4
William Mayton <william_mayton@Williams-MacBook-Air.local>
2021-04-06 17:14:26 -0400

    python update- PyBank close to complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,11 +3,56 @@ import os
 #import module for reading CSV files
 import csv
 csvpath = os.path.join('Resources', 'PyBank.csv')
-total_months = -1
-for row in open(csvpath):
-    total_months += 1
-print (total_months)
+total_months = 0
+with open(csvpath) as fin:
+    headerline = next(fin)
+    total_profit = 0
+    Dates, Values = [], []
+    Value_Change = 0
+    for i in csv.reader(fin):
+        total_months += 1
+        total_profit += int(i[1])
+        Dates.append(i[0])
+        # Values.append(i[1])
+        Values.append(int(i[1]) - Value_Change)
+        Value_Change = (int(i[1]))
+    Values.pop(0)
+    Dates.pop(0)
+    t = (sum(Values) / (total_months - 1))
+    print(min(Values))
+    print(max(Values))
+    r = Values.index(min(Values))
+    s = Values.index(max(Values))
+    print(total_months)
+    print(Dates[r])
+    print(Dates[s])
+    print(t)
+    print (total_profit)
 
-with open(csvpath) as f:
-    rows = csv.DictReader(f)
-    print sum(float(r['1']) for r in rows)
+
+
+
+
+# #get max/min values for biggest profit/loss
+# with open(csvpath) as csvfile:
+#     data = csv.reader(csvfile, delimiter=';')
+#     minVal, maxVal = [], []
+#     for i in data:
+#         minVal.append(i[1])
+#         maxVal.append(i[2])
+
+# print min (minVal)
+# print max (maxVal)
+
+
+# print ("Financial Analysis")
+# print ("------------------")
+# print (f"Total Months:{int(total_months)}")
+# print 
+
+
+# #export analysis to .txt file
+# with open('PyBank.txt,' "w") as text:
+#     text.write("Financial Analysis /n")
+#     text.write ("---------- /n")
+#     text.write (f"Total Months:{int(total_months)}"")
--- PyBank/main2.py
+++ PyBank/main2.py
@@ -2,8 +2,8 @@ import os
 import csv
 csvpath = os.path.join('Resources', 'PyBank.csv')
 with open(csvpath) as fin:
-    headerline = fin.next()
-    total = 0
+    headerline = next(fin)
+    total_profit = 0
     for row in csv.reader(fin):
-        total_profit = int(row[1])
+        total_profit += int(row[1])
     print (total_profit)
